6cb684f73a70ccafce0cde61890f7d05db14203f
Debasish Bhola <devop69@gmail.com>
2023-07-15 20:08:34 +0530

    Update Main file
    New Features added

--- app/src/main/java/com/example/app_basicphrase/MainActivity.java
+++ app/src/main/java/com/example/app_basicphrase/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.app_basicphrase;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
@@ -12,6 +13,12 @@ public class MainActivity extends AppCompatActivity {
         int id = view.getId();
         String ourId = "";
         ourId = view.getResources().getResourceEntryName(id);
+
+        int resourceId = getResources().getIdentifier(ourId,"raw","com.example.app_basicphrase");
+
+        MediaPlayer mplayer = MediaPlayer.create(this, resourceId);
+        mplayer.start();
+
         Log.i("button tapped", ourId);
     }
 
